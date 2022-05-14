42542a9739013e4138cce42628bf4d43b5fb6e90
StepanKarpyak <stepan_karpyak@ukr.net>
2022-05-14 21:30:41 +0300

    тз правки

--- src/index.js
+++ src/index.js
@@ -10,24 +10,3 @@
 // );
 
 
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'
-import './index.css';
-import App from './components/App';
-import { store, persistor } from './redux/store';
-
-
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-        </PersistGate>
-    </Provider>
-  </React.StrictMode>,
-);
